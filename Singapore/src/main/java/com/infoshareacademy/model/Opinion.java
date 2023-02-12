58735d79b8e95cb70a833705de33c7211e027d30
maciekserwin <maciekserwin@gmail.com>
2023-02-12 12:05:01 +0100

    dodawanie klas etap 1

---
+++ Singapore/src/main/java/com/infoshareacademy/model/Opinion.java
@@ -0,0 +1,16 @@
+package com.infoshareacademy.model;
+
+import com.infoshareacademy.place.Town;
+
+public class Opinion {
+
+    private String comment;
+
+    private int rate;
+
+    private User user;
+
+    private Town town;
+
+
+}
---
+++ Singapore/src/main/java/com/infoshareacademy/model/Trip.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.model;
+
+public class Trip extends TripGenerator {
+
+    private int id;
+
+    private TripGenerator tripGenerator;
+
+
+
+}
