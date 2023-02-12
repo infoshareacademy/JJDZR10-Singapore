ec7f3eb71cd9b307fa32cf4a2eaa6f6609b491ec
maciekserwin <maciekserwin@gmail.com>
2023-02-12 12:34:10 +0100

    dodawanie klas etap 2

---
+++ Singapore/src/main/java/com/infoshareacademy/place/Nature.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.place;
+
+public class Nature {
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Double rate;
+
+}
--- Singapore/src/main/java/com/infoshareacademy/place/Town.java
+++ Singapore/src/main/java/com/infoshareacademy/place/Town.java
@@ -1,4 +1,14 @@
 package com.infoshareacademy.place;
 
+import java.util.List;
+
 public class Town {
+    private Monuments monumets;
+
+    private Nature nature;
+
+    private Restaurants restaurnts;
+
+
+
 }
