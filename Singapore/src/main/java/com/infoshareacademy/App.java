d96f0c60e3c6764cde909d1d24b531693066c1fa
maciekserwin <maciekserwin@gmail.com>
2023-02-12 13:51:38 +0100

    zapis danych do pliku etap 2

--- Singapore/src/main/java/com/infoshareacademy/App.java
+++ Singapore/src/main/java/com/infoshareacademy/App.java
@@ -1,13 +1,18 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.service.ReadFile;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        ReadFile readFile = new ReadFile();
+        readFile.readFile();
+
     }
+
 }
