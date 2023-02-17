972c3d47f9274a608b2e6ba4335b0cd1af73ac41
BartoszLubak <barteklubak@gmail.com>
2023-02-17 18:57:29 +0100

    odczyt fragmentu tekstu z pliku i zapis na jego końcu

--- Singapore/src/main/java/com/infoshareacademy/service/ReadFile.java
+++ Singapore/src/main/java/com/infoshareacademy/service/ReadFile.java
@@ -1,13 +1,15 @@
 package com.infoshareacademy.service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+
+import java.nio.Buffer;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ReadFile {
-
-    private String writeFile;
+    private String readFile;
+    private File file;
+    private String userInput;
     private String filePath = "/home/bartek/Desktop/Singapore/JJDZR10-Singapore/Singapore/src/main/resources/Gdańsk/Monuments/próba.txt";
     // TODO Stworzyć metodę na generowanie ścieżki
 
@@ -16,35 +18,49 @@ public class ReadFile {
         this.filePath = filePath;
     }
 
-    public String readFile() {
-        File file = new File(filePath);
-        Scanner in = null;
-        String zdanie = "";
+    public void readFile() {
+        file = new File(filePath);   //file - zmienna z nazwą miejsca
+        String userInput = null;
+
         try {
-            in = new Scanner(file);
-            zdanie = in.nextLine();
-            System.out.println(zdanie);
+            Scanner in = new Scanner(file);
+            userInput = in.nextLine();
+            while (in.hasNextLine()) {
+                userInput = in.nextLine();
+                if (userInput.equals("Komentarz:")) {
+                    do{
+                        userInput=in.nextLine();
+                        System.out.println(userInput);
+                    }
+                    while (!in.hasNext("%koniec%"));
+
+                }
+            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        return zdanie;
     }
+
+
 
     public void setWriteFile(String writeFile) {
-        this.writeFile = writeFile;
+        this.userInput = writeFile;
     }
 
-    public void writeFile() {    //metoda do zapisu w tekstu pliku
-        PrintWriter write;
-
+    public void writeFile() {    //metoda zapisu na końcu pliku tekstowego
+        file = new File(filePath); //file - zmienna z nazwą pliku
         try {
-            write = new PrintWriter(filePath);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            FileWriter fileWriter = new FileWriter(filePath, true);
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.nextLine();
+            fileWriter.write(userInput);
+            // fileWriter.write(userLogin);   //TODO dopisanie na końcu komentarza loginu użytkownika
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
 
-        write.println(writeFile);
-        write.close();
+
         System.out.println("Dodanie tekstu zakończone sukcesem");
     }
 
