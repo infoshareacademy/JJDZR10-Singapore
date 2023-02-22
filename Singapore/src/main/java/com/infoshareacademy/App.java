b4b4580d14798c1123a6cf25e29da56f365c526e
Karolina Świstowska <karoli.swistowska@gmail.com>
2023-02-22 01:25:39 +0100

    [JJDZR10S-8] Basic JSON examples for Trip and User. Also dataaccess.Reader class to read data from json files and parse them to Model Class List

--- Singapore/src/main/java/com/infoshareacademy/App.java
+++ Singapore/src/main/java/com/infoshareacademy/App.java
@@ -1,13 +1,22 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.model.Persistent;
+import com.infoshareacademy.model.Trip;
+import com.infoshareacademy.service.dataacces.Reader;
+import org.json.simple.JSONObject;
+
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Reader reader = new Reader();
+        List<Persistent> trips = reader.getList(Trip.class);
+        for (Persistent o: trips) {
+            Trip oParsed = (Trip) o;
+            System.out.println(oParsed);
+        }
     }
 }
--- Singapore/src/main/java/com/infoshareacademy/model/Trip.java
+++ Singapore/src/main/java/com/infoshareacademy/model/Trip.java
@@ -1,14 +1,81 @@
 package com.infoshareacademy.model;
 
-import com.infoshareacademy.service.AddUserTrip;
-import com.infoshareacademy.service.TripGenerator;
+import com.infoshareacademy.model.place.Places;
+import java.util.List;
 
-public class Trip extends TripGenerator {
+public class Trip implements Persistent{
 
-    private int id;
+    private long id;
 
-    private TripGenerator tripGenerator;
+    private String name;
 
-    private AddUserTrip addUserTrip;
+    /**
+     * Distance in kilometers
+     */
+    private Double distance;
 
+    /**
+     * Time to spend in hours i.e. 5.5
+     */
+    private Double time_for_trip;
+
+    private User created_by_user;
+
+    public User getCreatedByUser() {
+        return created_by_user;
+    }
+
+    public void setCreatedByUser(User created_by_user) {
+        this.created_by_user = created_by_user;
+    }
+
+    /**
+     * Places to see on the route
+     */
+    private List<Places> places;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public double getTimeForTrip() {
+        return time_for_trip;
+    }
+
+    public void setTimeForTrip(double time_for_trip) {
+        this.time_for_trip = time_for_trip;
+    }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", distance=" + distance +
+                ", time_for_trip=" + time_for_trip +
+                ", created_by_user=" + created_by_user +
+                ", places=" + places +
+                '}';
+    }
 }
