7d05ec2fa5f58426e775762ba6c7f0bf27427125
BartoszLubak <barteklubak@gmail.com>
2023-02-22 22:52:41 +0100

    Tworzenie komentarza do obiektu, obliczanie średniej oceny.

--- Singapore/src/main/java/com/infoshareacademy/model/User.java
+++ Singapore/src/main/java/com/infoshareacademy/model/User.java
@@ -9,6 +9,24 @@ public class User extends PersistentAbstract {
 
     private String lastName;
 
+    public User(String login, String firstName, String lastName) {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
 
     public String getLogin() {
         return login;
