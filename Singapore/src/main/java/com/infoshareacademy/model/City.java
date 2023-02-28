1f18247f65aeeca04a87d6f80125a2dfd266e1e1
kbledzki <krzysztofbledzki@gmail.com>
2023-02-28 19:24:26 +0100

    Wyswietlenie list dostepnych miejsc i miast cz.1

--- Singapore/src/main/java/com/infoshareacademy/model/City.java
+++ Singapore/src/main/java/com/infoshareacademy/model/City.java
@@ -3,4 +3,9 @@ package com.infoshareacademy.model;
 public class City extends PersistentAbstract {
     private String name;
     private String description;
+
+    @Override
+    public void setId(long id) {
+
+    }
 }
