775a11452227bebbf38eae1b885bb37a382a5e1a
Karolina Świstowska <karoli.swistowska@gmail.com>
2023-03-03 22:39:25 +0100

    [JJDZR10S-8] Adjust Place and City

--- Singapore/src/main/java/com/infoshareacademy/model/Place.java
+++ Singapore/src/main/java/com/infoshareacademy/model/Place.java
@@ -1,9 +1,20 @@
 package com.infoshareacademy.model;
 
-public class Place implements Persistent{
+import org.json.simple.JSONObject;
+
+public class Place extends PersistentAbstract{
     private String name;
     private String description;
     private double prize;
     private double rate;
     private String opinion;
+
+    private City city;
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject jsonObject = super.toJSON();
+        jsonObject.put("city",city.getId());
+        return jsonObject;
+    }
 }
