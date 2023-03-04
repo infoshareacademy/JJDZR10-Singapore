ecac286105fadc62e2c965c1961e52dbf3fba8fa
BartoszLubak <barteklubak@gmail.com>
2023-03-04 22:56:17 +0100

    poprawa kompilacji - zakomentowanie json reader w OpoinionService

--- Singapore/src/main/java/com/infoshareacademy/service/OpinionService.java
+++ Singapore/src/main/java/com/infoshareacademy/service/OpinionService.java
@@ -26,7 +26,7 @@ public class OpinionService extends ValidatorService implements Persistent {
         scanUserString("Napisz komentarz.", "Nic nie napisałeś, podaj swoją opinię");
         setRate();
         System.out.println("Komentarz dodano. ");
-        userOpinion = "Komentarz:\n" + getUserScanString() + "\nod: " + user.getLogin() + "\nŚrednia ocena " + ratingsList.size() + " użytkowników to " + objectRate + ".";
+        userOpinion = "Komentarz:\n" + getUserScanString() + "\nod: " + user.getLogin();
         System.out.println(userOpinion);  //FIXME usunąć po scaleniu z zapisywaniem w json
         return userOpinion;
 
@@ -35,11 +35,11 @@ public class OpinionService extends ValidatorService implements Persistent {
     private Integer setRate() {
         userScanInteger("Podaj swoją ocenę w skali 1-10", "Podałeś liczbę spoza zakresu", 1, 10);
         ratingsList.add(getUserScanInteger());
-        getObjectRate();
+        setObjectRate();
         return userRate = getUserScanInteger();
     }
 
-    public Double getObjectRate() {
+    private Double setObjectRate() {
         Double sum = 0d;
         for (int i = 0; i < ratingsList.size(); i++) {
             sum += (double) ratingsList.get(i);
@@ -47,22 +47,22 @@ public class OpinionService extends ValidatorService implements Persistent {
         return objectRate = (double) (sum / ratingsList.size());
     }
 
-    public void opinionFilter (){
-
-        Reader reader = new Reader();
-        List opinionList = reader.getList(OpinionService.class);
-
-        Integer rate = userScanInteger("Podaj minimalną ocenę komentarza", "Podałeś liczbę spoza zakresu. Wprowadź ją ponownie", 1, 10);
-        List<String> reducedList = opinionList.stream()
-                .filter(o -> o.getUserRate() > rate)
-                .map(o->o.getUserOpinion()).collect(Collectors.toList());
-
-        Integer opinionsToShow = userScanInteger("Podaj ilość opinii do wyświetlenie", "Nie mamy tylu opinii", 1, reducedList.size());
-        for (int i=0; i<opinionsToShow; i++){
-                System.out.println(reducedList.get(i));
-                System.out.println();
-            }
-        }
+//    public void opinionFilter (){
+//
+//        Reader reader = new Reader();
+//        List opinionList = reader.getList(OpinionService.class);
+//
+//        Integer rate = userScanInteger("Podaj minimalną ocenę komentarza", "Podałeś liczbę spoza zakresu. Wprowadź ją ponownie", 1, 10);
+//        List<String> reducedList = opinionList.stream()
+//                .filter(o -> o.getUserRate() > rate)
+//                .map(o->o.getUserOpinion()).collect(Collectors.toList());
+//
+//        Integer opinionsToShow = userScanInteger("Podaj ilość opinii do wyświetlenie", "Nie mamy tylu opinii", 1, reducedList.size());
+//        for (int i=0; i<opinionsToShow; i++){
+//                System.out.println(reducedList.get(i));
+//                System.out.println();
+//            }
+//        }
 
 
 
@@ -76,6 +76,11 @@ public class OpinionService extends ValidatorService implements Persistent {
 
     public Integer getUserRate() {
         return userRate;
+    }
+
+    public Double getObjectRate() {
+        System.out.println( "\nŚrednia ocena " + ratingsList.size() + " użytkowników to " + objectRate + ".");
+        return objectRate;
     }
 
     @Override
