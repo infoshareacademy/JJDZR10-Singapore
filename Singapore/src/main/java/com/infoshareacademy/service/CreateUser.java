3c4b0592e9013800731896fa14a5ccd900cb249c
maciekserwin <maciekserwin@gmail.com>
2023-03-11 23:55:42 +0100

    start createuser

---
+++ Singapore/src/main/java/com/infoshareacademy/service/CreateUser.java
@@ -0,0 +1,15 @@
+package com.infoshareacademy.service;
+
+import java.util.Scanner;
+
+public class CreateUser {
+    Scanner inputUser = new Scanner(System.in);
+    String name;
+    String surname;
+    String login;
+    String password;
+
+
+
+
+}
