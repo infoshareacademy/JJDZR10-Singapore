d6891eec30e32e93b8a9ecdfc45396efc69db905
maciekserwin <maciekserwin@gmail.com>
2023-03-11 21:26:31 +0100

    try resolve conflict

--- Singapore/src/main/java/com/infoshareacademy/App.java
+++ Singapore/src/main/java/com/infoshareacademy/App.java
@@ -1,20 +1,16 @@
 package com.infoshareacademy;
-import com.infoshareacademy.service.ReadFile;
-import static com.infoshareacademy.service.Menu.OpenMenuMethod;
 
-/**
- * Hello world!
- *
- */
+
+import com.infoshareacademy.service.Menu;
+
 public class App {
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.OpenMenuMethod();
 
-        OpenMenuMethod();
-
-        ReadFile readFile = new ReadFile();
-        readFile.readFile();
 
 
     }
 
 }
+
