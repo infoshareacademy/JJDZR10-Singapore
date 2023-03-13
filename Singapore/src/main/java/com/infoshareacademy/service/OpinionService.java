49849065af93c83a138aef1c0a0afcb50cc7a0bc
BartoszLubak <barteklubak@gmail.com>
2023-03-13 21:19:05 +0100

    zmiana skrzynki pocztowej

--- Singapore/src/main/java/com/infoshareacademy/service/OpinionService.java
+++ Singapore/src/main/java/com/infoshareacademy/service/OpinionService.java
@@ -7,38 +7,46 @@ import java.util.List;
 
 
 public class OpinionService extends ValidatorService {
+    private long id;
     private String userOpinion;
     private Double objectRate;
     private List<Integer> ratingsList = new ArrayList<>();
     private User user;
 
-    public OpinionService(User user) {
-        this.user = user;
+    public String getUserOpinion() {
+        return userOpinion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public List<Integer> getRatingsList() {
+        return ratingsList;
     }
 
     public String setUserOpinion() {
         scanUserString("Napisz komentarz.", "Nic nie napisałeś, podaj swoją opinię");
-        setRate();
         System.out.println("Komentarz dodano. ");
         userOpinion = "Komentarz:\n" + getUserScanString() + "\nod: " + user.getLogin() + "\nŚrednia ocena " + ratingsList.size() + " użytkowników to " + objectRate + ".";
-        System.out.println(userOpinion);  //FIXME usunąć po scaleniu z zapisywaniem w json
         return userOpinion;
-
     }
 
-    private void setRate() {
+    public void setRate() {
         userScanInteger("Podaj swoją ocenę w skali 1-10", "Podałeś liczbę spoza zakresu", 1, 10);
         ratingsList.add(getUserScanInteger());
-        getObjectRate();
+        objectRate();
     }
 
-    private Double getObjectRate() {
+    public Double objectRate() {
         Double sum = 0d;
         for (int i = 0; i < ratingsList.size(); i++) {
             sum += (double) ratingsList.get(i);
         }
         return objectRate = (double) (sum / ratingsList.size());
     }
+
+
 }
 
 
