eb4d15e54f2da204f2c52145a25fffbf37371738
kbledzki <krzysztofbledzki@gmail.com>
2023-03-13 21:03:17 +0100

    Project Spring Done!

---
+++ TripPlaner/src/test/java/com/singapore/TripPlaner/TripPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singapore.TripPlaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
