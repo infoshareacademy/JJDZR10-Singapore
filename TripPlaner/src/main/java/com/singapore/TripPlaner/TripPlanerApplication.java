a49078538eaa28ac0168feddf665d0862553f4a4
BartoszLubak <barteklubak@gmail.com>
2023-03-16 23:34:30 +0100

    zapis do jsona

--- TripPlaner/src/main/java/com/singapore/TripPlaner/TripPlanerApplication.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/TripPlanerApplication.java
@@ -1,22 +1,13 @@
 package com.singapore.TripPlaner;
 
-import com.singapore.TripPlaner.Model.Opinion;
-import com.singapore.TripPlaner.Service.dataacces.Writer;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripPlanerApplication {
     public static void main(String[] args) {
 
-        Opinion opinion = new Opinion();
-        opinion.setId_user(0);
-        opinion.setUserOpinion("Super");
-        opinion.setUserRate(4);
-        Writer writer = new Writer();
-        writer.save(opinion);
-
-
-//		SpringApplication.run(TripPlanerApplication.class, args);
+		SpringApplication.run(TripPlanerApplication.class, args);
     }
 
 }
