9df41eb101a294515deb2cb147fa407ea4b980ae
BartoszLubak <barteklubak@gmail.com>
2023-04-07 12:29:01 +0200

    create setters/getters and Places.java, create pins on the map

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Controller/MapController.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Controller/MapController.java
@@ -1,6 +1,7 @@
 package com.singapore.TripPlaner.Controller;
 
 import com.singapore.TripPlaner.Model.Places;
+import com.singapore.TripPlaner.Service.PlaceService;
 import com.singapore.TripPlaner.Service.TripService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MapController {
 
     private TripService tripService;
+    private PlaceService placeService;
+    private Places places;
 
-    public MapController(TripService tripService) {
+    public MapController(TripService tripService, PlaceService placeService, Places places) {
         this.tripService = tripService;
+        this.placeService = placeService;
+        this.places = places;
     }
 
     @GetMapping("/map")
     public String getMap(Model model){
-        model.addAttribute("place", new Places());
+        model.addAttribute("point", placeService.getAllPlaces());
         return "map";
     }
 }
--- TripPlaner/src/main/java/com/singapore/TripPlaner/Model/Places.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Model/Places.java
@@ -1,7 +1,10 @@
 package com.singapore.TripPlaner.Model;
 
 import org.json.simple.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
+@Component
 public class Places extends PersistentAbstract {
 
     private String name;
@@ -13,6 +16,25 @@ public class Places extends PersistentAbstract {
     private long cityid;
 //    private int type;
     private Type type;
+    private double lat;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    private double lon;
 
     public Type getType() {
         return type;
