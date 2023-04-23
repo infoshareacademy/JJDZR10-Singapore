79ebc3ab11b2c21f33ead15dd09feb6a84a5ceec
maciekserwin <maciekserwin@gmail.com>
2023-04-23 02:39:29 +0200

    add annotations for city validation

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Model/City.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Model/City.java
@@ -4,12 +4,16 @@ import jdk.jfr.Description;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
 public class City extends PersistentAbstract {
-
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+    @NotBlank(message = "Describe cannot be empty")
+    @Size(min=10, max=55, message = "Describe shuold be betwen from 10 to 55 signs")
     private String description;
 
     public String getName() {
