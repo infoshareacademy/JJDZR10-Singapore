863364a346cf729b891e4035639fae1b902c5675
BartoszLubak <barteklubak@gmail.com>
2023-05-16 21:16:48 +0200

    delete lombok implementation

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Model/Image.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Model/Image.java
@@ -1,12 +1,23 @@
 package com.singapore.TripPlaner.Model;
 
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Image extends PersistentAbstract {
     private String url;
     private User user;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
