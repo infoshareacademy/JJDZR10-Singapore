3036b185421ec044ff3e5c6f2e920482629f8fd9
maciekserwin <maciekserwin@gmail.com>
2023-06-18 00:55:41 +0200

    add annotation of @: controller, service, autowired and requestmapping

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Service/EmailService.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Service/EmailService.java
@@ -1,15 +1,19 @@
 package com.singapore.TripPlaner.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+@Service
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Autowired
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
