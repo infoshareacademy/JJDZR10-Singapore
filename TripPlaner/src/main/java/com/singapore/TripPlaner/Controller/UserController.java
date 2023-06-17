ba44f8c7b512c499c728fe7e9c14d640a9c704fa
maciekserwin <maciekserwin@gmail.com>
2023-06-18 00:48:33 +0200

    injection of EmailService

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Controller/UserController.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Controller/UserController.java
@@ -1,5 +1,13 @@
 package com.singapore.TripPlaner.Controller;
 
+import com.singapore.TripPlaner.Service.EmailService;
+
 public class UserController {
+
+    private final EmailService emailService;
+
+    public UserController(EmailService emailService) {
+        this.emailService = emailService;
+    }
 
 }
