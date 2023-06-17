29405304f17b5952c5ddde7b3ca1c4de8c07ad19
maciekserwin <maciekserwin@gmail.com>
2023-06-18 00:37:32 +0200

    injection of JavaMailSender interfejs

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Service/EmailService.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Service/EmailService.java
@@ -1,5 +1,13 @@
 package com.singapore.TripPlaner.Service;
 
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
 }
