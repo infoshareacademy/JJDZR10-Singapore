a68759bb837e3818255e2ade155b0c9db0e95948
maciekserwin <maciekserwin@gmail.com>
2023-06-18 01:02:14 +0200

    add endpoint for registration of user

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Controller/UserController.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Controller/UserController.java
@@ -3,7 +3,9 @@ package com.singapore.TripPlaner.Controller;
 import com.singapore.TripPlaner.Service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -14,6 +16,18 @@ public class UserController {
     @Autowired
     public UserController(EmailService emailService) {
         this.emailService = emailService;
+    }
+
+    @GetMapping("/registration")
+    public String registerUser(@RequestParam String username, @RequestParam String password) {
+        // TODO Dodać logikę rejestracji użytkownika
+        String recipientEmail = "adres_odbiorcy@example.com";
+        String confirmationCode = "12345";
+
+        emailService.sendConfirmationEmail(recipientEmail, confirmationCode, username, password);
+
+        // TODO Przekirować na stronę potwierdzenia rejestracji
+        return "confirmation-page";
     }
 
 }
