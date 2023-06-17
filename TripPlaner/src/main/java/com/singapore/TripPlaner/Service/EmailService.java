74470f3a90c5ef8e6a2c482d8b258e0b30131fd3
maciekserwin <maciekserwin@gmail.com>
2023-06-18 00:43:53 +0200

    add implementation of JavaMailSender

--- TripPlaner/src/main/java/com/singapore/TripPlaner/Service/EmailService.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/Service/EmailService.java
@@ -1,6 +1,10 @@
 package com.singapore.TripPlaner.Service;
 
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public class EmailService {
 
@@ -10,4 +14,24 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    public void sendConfirmationEmail(String recipientEmail, String confirmationCode, String username, String password) {
+        String subject = "Potwierdzenie rejestracji";
+        String body = "Dziękujemy za rejestrację, " + username + "! Twój kod potwierdzający to: " + confirmationCode;
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        try {
+            helper.setTo(recipientEmail);
+            helper.setSubject(subject);
+            helper.setText(body);
+
+            mailSender.send(message);
+
+            System.out.println("Wysłano email potwierdzający rejestrację na adres: " + recipientEmail);
+
+        } catch (MessagingException e) {
+            System.out.println("Wystąpił błąd podczas wysyłania emaila: " + e.getMessage());
+        }
+    }
 }
