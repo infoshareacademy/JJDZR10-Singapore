48fe9f7455e77d7196515238b0282e7c1381f274
Karolina Świstowska <karoli.swistowska@gmail.com>
2023-06-30 23:02:32 +0200

    Dodanie context

--- TripPlaner/src/main/java/com/singapore/TripPlaner/TripPlanerApplication.java
+++ TripPlaner/src/main/java/com/singapore/TripPlaner/TripPlanerApplication.java
@@ -2,12 +2,14 @@ package com.singapore.TripPlaner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TripPlanerApplication {
+    private static ApplicationContext applicationContext;
     public static void main(String[] args) {
 
-		SpringApplication.run(TripPlanerApplication.class, args);
+        applicationContext=SpringApplication.run(TripPlanerApplication.class, args);
     }
 
 }
